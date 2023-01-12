55154dd84029001d2aabc41f8c1aa326e00076f9
Umid Amirkhulov <uamirqulov5@gmail.com>
2023-01-12 08:21:27 -0700

    learn javascript

---
+++ js/script.js
@@ -0,0 +1,10 @@
+console.log('Hello Javasciript')
+let educationCentre = 'Global Education'
+let univercity = 'Oxford'
+let phone = 'Samsung'
+let count = 25
+let dev = 'Frontend'
+let learn = 'Javascript'
+console.log(educationCentre, dev)
+console.log(phone)
+console.log(univercity)
